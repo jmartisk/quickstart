18588da19bf7e1dfa6aaca6f23b52293f2ec9af6
Jan Martiska <jmartisk@redhat.com>
2017-11-27 14:13:51 +0100

    [JBEAP-13839 ejb-security-programmatic-auth fix dependencies; add remote.ejb.host property

--- ejb-security-programmatic-auth/src/main/java/org/jboss/as/quickstarts/ejb_security_programmatic_auth/RemoteClient.java
+++ ejb-security-programmatic-auth/src/main/java/org/jboss/as/quickstarts/ejb_security_programmatic_auth/RemoteClient.java
@@ -55,7 +55,7 @@ public class RemoteClient {
     static final Callable<String> callable = () -> {
         final Hashtable<String, String> jndiProperties = new Hashtable<>();
         jndiProperties.put(Context.INITIAL_CONTEXT_FACTORY, "org.wildfly.naming.client.WildFlyInitialContextFactory");
-        jndiProperties.put(Context.PROVIDER_URL, "remote+http://localhost:8080");
+        jndiProperties.put(Context.PROVIDER_URL, "remote+http://" + System.getProperty("remote.ejb.host", "localhost") + ":8080");
         final Context context = new InitialContext(jndiProperties);
 
         SecuredEJBRemote reference = (SecuredEJBRemote) context.lookup("ejb:/ejb-security-programmatic-auth/SecuredEJB!"
